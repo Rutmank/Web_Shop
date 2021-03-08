8feea89635cc8c6fcc98840abe51d180a841353e
Rutmank <rutmank@yandex.ru>
2021-03-08 20:00:39 +0200

    Update

--- Web_Shop/Data/Models/Categories.cs
+++ Web_Shop/Data/Models/Categories.cs
@@ -7,5 +7,13 @@ namespace Web_Shop.Data.Models
 {
     public class Categories
     {
+        public int id { set; get; } // Поле id
+
+        public string categoryName { set; get; } // Поле имения
+
+        public string description { set; get; } // Поле описания
+
+       // public int id { set; get; }
+
     }
 }

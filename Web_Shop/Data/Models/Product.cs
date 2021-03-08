8ab75332a6e845671200457480c1cb14406f1f61
Rutmank <rutmank@yandex.ru>
2021-03-08 20:11:03 +0200

    Update

--- Web_Shop/Data/Models/Product.cs
+++ Web_Shop/Data/Models/Product.cs
@@ -7,6 +7,24 @@ namespace Web_Shop.Data.Models
 {
     public class Product
     {
+        public int id { set; get; } // Id товара
 
+        public string name  { set; get; } // Имя товара
+
+        public string shortDesc { set; get; } // Короткое описаниа товара
+
+        public string longDesc { set; get; } // Длинное описание товара
+
+        public string img { set; get; } // Внутрь url изображения товара
+
+        public ushort price { set; get; } // Цена товара. ushort - потому что цена всегда положительная
+
+        public bool isFacourite { set; get; } // Указание товара на главную страницу
+
+        public int availlable { set; get; } // Количество товара 
+
+        public int categoryID { set; get; } // Прикрепление товара к определенной категории 
+
+        public virtual Categories Category { set; get; } // Создается объект со всеми доступными категориями из categories
     }
 }

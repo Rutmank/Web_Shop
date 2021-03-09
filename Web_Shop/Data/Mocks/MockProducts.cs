fa4b960dc11f7aae41f266f409488aa57090c5a0
Rutmank <rutmank@yandex.ru>
2021-03-09 20:35:43 +0200

    Update

---
+++ Web_Shop/Data/Mocks/MockProducts.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web_Shop.Data.Interfaces;
+using Web_Shop.Data.Models;
+
+namespace Web_Shop.Data.Mocks
+{
+    public class MockProducts : IAllProducts
+    {
+        public IEnumerable<Product> Products
+        {
+            get
+            {
+                return new List<Product>
+                {
+                    new Product{name:"Milk Valio", }
+                };
+            }
+        }
+        public IEnumerable<Product> getFavouriteProducts { get; set; }
+
+        public Product getObjectProduct(int productID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

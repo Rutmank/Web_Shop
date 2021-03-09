d718ecc947962940f364c6c528405679b0d7fd25
Rutmank <rutmank@yandex.ru>
2021-03-09 20:21:38 +0200

    Update

---
+++ Web_Shop/Data/Mocks/MockCategory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web_Shop.Data.Interfaces;
+using Web_Shop.Data.Models;
+
+namespace Web_Shop.Data.Mocks
+{
+    public class MockCategory : IProductCategory // Данный класс реалезует созданный интерфейс
+    {
+        public IEnumerable<Categories> AllCategories
+        { // Реализация интерефейса
+            get
+            {
+                return new List<Categories> // Возвращает лист с категориями 
+                {
+                    new Categories { categoryName = "Dairy products", description = "Milk, kefir, cottage cheese" }, // Созданные категории с описанием
+                    new Categories { categoryName = "Bakery", description = "Bread and bread products" }
+                };
+            }
+        }
+    }
+}

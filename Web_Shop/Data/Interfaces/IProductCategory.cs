1d653022cb00f147894b6b94dd50fd717dcf93f6
Rutmank <rutmank@yandex.ru>
2021-03-09 17:57:28 +0200

    Update

--- Web_Shop/Data/Interfaces/IProductCategory.cs
+++ Web_Shop/Data/Interfaces/IProductCategory.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Web_Shop.Data.Models;
 
 namespace Web_Shop.Data.Interfaces
 {
     interface IProductCategory
     {
+
+        IEnumerable<Categories> AllCategories { get; } // Функция, получающая данные из раздела категорий
+
     }
 }

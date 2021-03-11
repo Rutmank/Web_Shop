ef50f1fed9ecc765871b4f5817cd1b0ba22d60ce
Rutmank <rutmank@yandex.ru>
2021-03-11 20:52:30 +0200

    Update

--- Web_Shop/Controllers/ProductsController.cs
+++ Web_Shop/Controllers/ProductsController.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Web_Shop.Controllers
 {
-    public class ProductsController
-    {
+    public class ProductsController : Controller { // Наследует класс Контроллер
+
+
+
     }
 }

179f956bd380c47076782efa46349752d5ca49a6
Rutmank <rutmank@yandex.ru>
2021-03-13 19:07:38 +0200

    Update

--- Web_Shop/Controllers/ProductsController.cs
+++ Web_Shop/Controllers/ProductsController.cs
@@ -3,12 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Web_Shop.Data.Interfaces;
 
 namespace Web_Shop.Controllers
 {
     public class ProductsController : Controller { // Наследует класс Контроллер
 
-
+        private readonly IAllProducts _allProducts;
+        private readonly IProductCategory _allCategories;
 
     }
 }

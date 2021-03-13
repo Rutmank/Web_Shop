116638a0dd014ce176942a999d01ee7fc669cec3
Rutmank <rutmank@yandex.ru>
2021-03-13 19:17:29 +0200

    Update

--- Web_Shop/Controllers/ProductsController.cs
+++ Web_Shop/Controllers/ProductsController.cs
@@ -9,13 +9,19 @@ namespace Web_Shop.Controllers
 {
     public class ProductsController : Controller { // Наследует класс Контроллер
 
-        private readonly IAllProducts _allProducts;
+        private readonly IAllProducts _allProducts; // Переменные, передающие интерфейсы 
         private readonly IProductCategory _allCategories;
 
-        public ProductsController(IAllProducts iAllProducts, IProductCategory iproductCategory)
+        public ProductsController(IAllProducts iAllProducts, IProductCategory iproductCategory) // Передается интерфейс, а также класс, который его реализует
         {
             _allProducts = iAllProducts;
             _allCategories = iproductCategory;
+        }
+
+        public ViewResult List() // ViewResult возвращает HTML страницу. Функция возвращает список всех товаров
+        {
+            var products = _allProducts.Products; // Переменная наполняется данными 
+            return View(products); // Переменная передается в шаблок
         }
 
     }

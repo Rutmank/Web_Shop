94bcdb12213fe5b20329705d23e613934a0cdebe
Rutmank <rutmank@yandex.ru>
2021-03-19 16:22:56 +0200

    update

--- Web_Shop/ViewModels/ProductsListViewModel.cs
+++ Web_Shop/ViewModels/ProductsListViewModel.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Web_Shop.Data.Models;
 
 namespace Web_Shop.ViewModels
 {
     public class ProductsListViewModel
     {
+        public IEnumerable<Product> getAllProducts { get; set; }
     }
 }
